500742f1e8029a36af3ba28c4f2a6a865e7ac497
Rayhan <rayhanhrussell@gmail.com>
2025-10-02 08:05:47 +0600

    event handler reset and grid  drawing added

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ const colorSwitch = document.getElementById("colorSwitch");
 
 
 const DEFAULTCOLOR = "#000000"
+let isDrawing = false;
+let isEraseing = false;
+let crrentSize = 16;
+let currentColor = DEFAULTCOLOR;
+let colorDrawing = false;
+
+//Event handlers
+sizeButton.addEventListener("click",()=> {dialog.showModal()});
+closs.addEventListener("click"()=> dialog.close());
+
+
+//Reset Feature added
+resetButton.addEventListener("click", ()=>{
+    //removeGrid();
+    //createGrid(currentSize)
+    document.querySelectorAll("div.cell").forEach((item)=> {
+        item.style.backgroundColor = ""white";
+    });
+
+    });
+
+eraseButton.addEventListener("click", (e)=> eraseDrawing(e));
+eraseButton.addEventListener("dblclick", (e)=>eraseDrawing(e));
+colorSwitch.addEventListener("change", changeColor);
+
+//Grid drawing function
+function createGrid(gSize){
+    for (let i=0; i<(gSize*gSize); i++)
+    {
+        let gridCell = document.createElement("div");
+        gridCell.style.height = `${(600/gSize)-0.25}px`;
+        gridCell.style.width = `${(600/gSize)-0.25}px`;
+        gridCell.classList.add("cell");
+        gridCell.addEventListener("mousedown",(e)=> draw(e));
+        gridCell.addEventListener("mouseover", (e)=> draw(e));
+        gridCell.addEventListener("mouseup",(e)=> draw(e));
+        gridCell.addEventListener("drag", (e)=> draw(e));
+        gridCell.addEventListener("mouseover", (e)=> erase(e));
+        myGrid.appendChild(gridCell);
+    }
+}
